cddc6bf4204401b38e1fe7847c1831c368da1162
Szika05 <s.szili2005@gmail.com>
2026-01-21 02:19:34 +0100

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
